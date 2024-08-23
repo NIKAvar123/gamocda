1701046650a58e682708539a6770ac9f7950626b
NIKAvar123 <bastibubu31@gmail.com>
2024-08-23 18:00:27 +0400

    Add files via upload

---
+++ gamocda/gamocda.py
@@ -0,0 +1,24 @@
+print("bank")
+account=(input("please enter your name:"))
+password=(input("please enter your password:"))
+if password :
+    password=("nika123")
+    print("password true")
+    print("------------")
+
+print("menu")
+print("your money=800$")
+money_deposit=(input("deposit money:"))
+if money_deposit:
+    money_deposit=200
+    money_deposit=(input("please enter your phone number:"))
+    print("your money=1000$")
+
+print("--------")
+
+Withdraw_money=(input("Withdraw_money:"))
+if Withdraw_money:
+    Withdraw_money=100
+    print("your money=900$")
+
+print(exit)
